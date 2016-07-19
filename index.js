5feeb0eb7aa2aeafe0b6d6542245d065da93e88a
Jon Church <me@jonchurch.com>
2016-07-18 20:06:43 -0400

    added an hourly route taking lat lon, which makes an axios request

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var $http = require('axios');
 var port = process.env.PORT || 3000;
+var apiKey = require('./config.js').apiKey;
 
 var server = express();
+var baseUrl = 'https://api.forecast.io/forecast/';
 
 //Middleware
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
-server.get('/test', function(req, res){
-	res.send('Test test!');
+//test route
+server.get('/forecast/hourly/:lat,:lon', function(req, res){
+	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
+			.then(function(response){
+				var resObj = {
+					latitude: response.data.latitude,
+					longitude: response.data.longitude,
+					hourly: response.data.hourly,
+				};
+			res.json(resObj);
+	}).catch(function(err){
+		console.log(err);
+		res.send('badddddd!!!');
+	});
 });
 
 server.listen(port, function(){
