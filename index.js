7ed5d85115894b3bf97a912882816dc5d2a35d07
Jon Church <me@jonchurch.com>
2016-07-18 21:06:04 -0400

    added getHourly and getMinutely routes

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
-//test route
 server.get('/forecast/hourly/:lat,:lon', function(req, res){
 	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
 			.then(function(response){
@@ -21,6 +20,36 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
 					latitude: response.data.latitude,
 					longitude: response.data.longitude,
 					hourly: response.data.hourly,
+				};
+			res.json(resObj);
+	}).catch(function(err){
+		console.log(err);
+		res.send('badddddd!!!');
+	});
+});
+
+server.get('/forecast/daily/:lat,:lon', function(req, res){
+	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
+			.then(function(response){
+				var resObj = {
+					latitude: response.data.latitude,
+					longitude: response.data.longitude,
+					daily: response.data.daily,
+				};
+			res.json(resObj);
+	}).catch(function(err){
+		console.log(err);
+		res.send('badddddd!!!');
+	});
+});
+
+server.get('/forecast/minutely/:lat,:lon', function(req, res){
+	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
+			.then(function(response){
+				var resObj = {
+					latitude: response.data.latitude,
+					longitude: response.data.longitude,
+					minutely: response.data.minutely,
 				};
 			res.json(resObj);
 	}).catch(function(err){
