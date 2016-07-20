e470cc59ba1eafdd4e9db097166ac0ebaef428eb
Jon Church <me@jonchurch.com>
2016-07-19 20:46:37 -0400

    added status codes to hourly, minutely, daily responses

--- index.js
+++ index.js
@@ -10,56 +10,61 @@ var baseUrl = 'https://api.forecast.io/forecast/';
 
 //Middleware
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cors());
 
-server.get('/forecast/hourly/:lat,:lon', function(req, res){
-	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
-			.then(function(response){
-				var resObj = {
-					latitude: response.data.latitude,
-					longitude: response.data.longitude,
-					hourly: response.data.hourly,
-				};
-			res.status(200).json(resObj);
-	}).catch(function(err){
-		console.log(err);
-		res.status(500).json({
-			msg: error
-		});
-	});
+server.get('/forecast/hourly/:lat,:lon', function(req, res) {
+    $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+        .then(function(response) {
+            var resObj = {
+                latitude: response.data.latitude,
+                longitude: response.data.longitude,
+                hourly: response.data.hourly
+            };
+            res.status(200).json(resObj);
+        }).catch(function(err) {
+            console.log(err);
+            res.status(500).json({
+                msg: err
+            });
+        });
 });
 
-server.get('/forecast/daily/:lat,:lon', function(req, res){
-	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
-			.then(function(response){
-				var resObj = {
-					latitude: response.data.latitude,
-					longitude: response.data.longitude,
-					daily: response.data.daily,
-				};
-			res.json(resObj);
-	}).catch(function(err){
-		console.log(err);
-		res.send('badddddd!!!');
-	});
+server.get('/forecast/daily/:lat,:lon', function(req, res) {
+    $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+        .then(function(response) {
+            var resObj = {
+                latitude: response.data.latitude,
+                longitude: response.data.longitude,
+                daily: response.data.daily
+            };
+            res.status(200).json(resObj);
+        }).catch(function(err) {
+            console.log(err);
+            res.status(500).json({
+                msg: err
+            });
+        });
 });
 
-server.get('/forecast/minutely/:lat,:lon', function(req, res){
-	$http.get(baseUrl + apiKey + '/' + req.params.lat +',' + req.params.lon)
-			.then(function(response){
-				var resObj = {
-					latitude: response.data.latitude,
-					longitude: response.data.longitude,
-					minutely: response.data.minutely,
-				};
-			res.json(resObj);
-	}).catch(function(err){
-		console.log(err);
-		res.send('badddddd!!!');
-	});
+server.get('/forecast/minutely/:lat,:lon', function(req, res) {
+    $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+        .then(function(response) {
+            var resObj = {
+                latitude: response.data.latitude,
+                longitude: response.data.longitude,
+                minutely: response.data.minutely
+            };
+            console.log(err);
+            res.status(200).send(resObj);
+        }).catch(function(err) {
+            console.log(err);
+            res.status(500).json({
+                msg: err
+            });
+        });
 });
 
-server.listen(port, function(){
-	console.log('Now listening on port....', port);
+server.listen(port, function() {
+    console.log('Now listening on port....', port);
 });
